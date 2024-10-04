e8bd80c6d523b163b3ca77d6019d3ca5aa897d17
Andrei Moldovan <123833398+MoldoAndr@users.noreply.github.com>
2024-10-04 18:12:37 +0300

    .

---
+++ pso/labs/lab1/malloc.c
@@ -0,0 +1,48 @@
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+static void* (*real_malloc)(size_t) = NULL;
+static void (*real_free)(void*) = NULL;
+
+void* malloc(size_t size) {
+    if (!real_malloc) {
+        real_malloc = dlsym(RTLD_NEXT, "malloc");
+        if (!real_malloc) {
+            const char *error = dlerror();
+            write(STDERR_FILENO, error, strlen(error));
+            _exit(EXIT_FAILURE);
+        }
+    }
+
+    void* ptr = real_malloc(size);
+
+    char buffer[256];
+    int len = snprintf(buffer, sizeof(buffer), "allocated %zu bytes at %p\n", size, ptr);
+    write(STDOUT_FILENO, buffer, len);
+
+    return ptr;
+}
+
+void free(void* ptr) {
+    if (!real_free) {
+        real_free = dlsym(RTLD_NEXT, "free");
+        if (!real_free) {
+            // Handle error
+            const char *error = dlerror();
+            write(STDERR_FILENO, error, strlen(error));
+            _exit(EXIT_FAILURE);
+        }
+    }
+
+    real_free(ptr);
+    
+    char buffer[256];
+    int len = snprintf(buffer, sizeof(buffer), "free memory at address %p\n", ptr);
+    write(STDOUT_FILENO, buffer, len);
+
+}
